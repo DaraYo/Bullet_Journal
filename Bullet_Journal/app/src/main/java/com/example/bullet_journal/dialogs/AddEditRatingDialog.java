23efc2e10d4871e8ee55ba610c42b7a4a6e9f825
mjok <marijajoksimovic@hotmail.com>
2019-05-28 14:12:49 +0200

    Rating, kategorije

--- Bullet_Journal/app/src/main/java/com/example/bullet_journal/dialogs/AddEditRatingDialog.java
+++ Bullet_Journal/app/src/main/java/com/example/bullet_journal/dialogs/AddEditRatingDialog.java
@@ -4,12 +4,19 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bullet_journal.R;
+import com.example.bullet_journal.adapters.RatingCategoryAdapter;
 import com.example.bullet_journal.model.Rating;
+import com.example.bullet_journal.wrapperClasses.RatingCategoryWrapper;
+
+import java.util.List;
 
 public class AddEditRatingDialog extends Dialog {
 
@@ -25,11 +32,16 @@ public class AddEditRatingDialog extends Dialog {
     private ImageButton ratingBtn4;
     private ImageButton ratingBtn5;
 
-    public AddEditRatingDialog(Context context, String selectedDate, Rating ratingObj) {
+    private List<RatingCategoryWrapper> categories;
+    private RatingCategoryAdapter categoryAdapter;
+
+    public AddEditRatingDialog(Context context, String selectedDate, Rating ratingObj, List<RatingCategoryWrapper> categories) {
         super(context);
+        this.context = context;
         this.selectedDate = selectedDate;
         this.ratingObj = ratingObj;
         this.rating = -1;
+        this.categories = categories;
     }
 
     @Override
@@ -101,7 +113,24 @@ public class AddEditRatingDialog extends Dialog {
             }
         });
 
+        Spinner categorySpinner = findViewById(R.id.rating_category_spinner);
+        categoryAdapter = new RatingCategoryAdapter(context, categories);
+        categorySpinner.setAdapter(categoryAdapter);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                RatingCategoryWrapper categoryObj = (RatingCategoryWrapper) parent.getItemAtPosition(position);
+                Toast.makeText(context, categoryObj.getCategory().toString(), Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
 
     private void resolveSelection(int newRating){
 
