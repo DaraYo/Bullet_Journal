7ed3b0d24cb75d5b750ebb9ad51b7d99e317e185
mjok <marijajoksimovic@hotmail.com>
2019-05-28 10:39:25 +0200

    Dodavanje mood-a, selekcija emotikona

--- Bullet_Journal/app/src/main/java/com/example/bullet_journal/dialogs/AddEditMoodDialog.java
+++ Bullet_Journal/app/src/main/java/com/example/bullet_journal/dialogs/AddEditMoodDialog.java
@@ -4,8 +4,11 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +24,11 @@ public class AddEditMoodDialog extends Dialog {
 
     private Context context;
     private long selectedDate;
+    private int selectedMoodVal;
     private Mood moodObj;
 
     private TextView dialogDescription;
+    private LinearLayout selectedView;
 
     private FirebaseFirestore firestore;
     private CollectionReference colRef;
@@ -32,6 +37,7 @@ public class AddEditMoodDialog extends Dialog {
         super(context);
         this.context = context;
         this.selectedDate = selectedDate;
+        this.selectedMoodVal = -1;
         this.moodObj = moodObj;
         this.firestore = FirebaseFirestore.getInstance();
         this.colRef = this.firestore.collection("Moods");
@@ -61,25 +67,74 @@ public class AddEditMoodDialog extends Dialog {
 
                 if(moodObj == null){
                     String description = dialogDescription.getText().toString();
-                    Mood mood = new Mood(selectedDate, 5, description);
+                    if(selectedMoodVal > 0){
+                        Mood mood = new Mood(selectedDate, selectedMoodVal, description);
 
-                    colRef.document().set(mood).addOnSuccessListener(
-                        new OnSuccessListener< Void >() {
+                        colRef.document().set(mood).addOnSuccessListener(
+                                new OnSuccessListener< Void >() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(context, "Mood saved", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        ).addOnFailureListener(new OnFailureListener() {
                             @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(context, "Mood saved", Toast.LENGTH_SHORT).show();
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(context, "Greska!", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                    ).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(context, "Greska!", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                        });
+                    }else{
+                        Toast.makeText(context, "Please select mood", Toast.LENGTH_SHORT).show();
+                    }
 
                 }
 
                 dismiss();
+            }
+        });
+
+        ImageButton awesomeImageBtn = (ImageButton) findViewById(R.id.mood_awesome_btn);
+        awesomeImageBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resolveSelection(5);
+            }
+        });
+
+        ImageButton goodImageBtn = (ImageButton) findViewById(R.id.mood_good_btn);
+        goodImageBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resolveSelection(4);
+            }
+        });
+
+        ImageButton okImageBtn = (ImageButton) findViewById(R.id.mood_ok_btn);
+        okImageBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resolveSelection(3);
+            }
+        });
+
+        ImageButton badImageBtn = (ImageButton) findViewById(R.id.mood_bad_btn);
+        badImageBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resolveSelection(2);
+            }
+        });
+
+        ImageButton terribleImageBtn = (ImageButton) findViewById(R.id.mood_terrible_btn);
+        terribleImageBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                resolveSelection(1);
             }
         });
 
@@ -91,4 +146,34 @@ public class AddEditMoodDialog extends Dialog {
             }
         });
     }
+
+    private void resolveSelection(int newMoodVal){
+
+        this.selectedMoodVal = newMoodVal;
+
+        if(selectedView != null){
+            selectedView.setBackgroundResource(0);
+        }
+
+        switch(this.selectedMoodVal) {
+            case (5) :
+                selectedView = findViewById(R.id.awesome_layout);
+                break;
+            case (4) :
+                selectedView = findViewById(R.id.good_layout);
+                break;
+            case (3) :
+                selectedView = findViewById(R.id.ok_layout);
+                break;
+            case (2) :
+                selectedView = findViewById(R.id.bad_layout);
+                break;
+            case (1) :
+                selectedView = findViewById(R.id.terrible_layout);
+                break;
+        }
+
+        selectedView.setBackground(ContextCompat.getDrawable(context, R.drawable.mood_border));
+    }
+
 }
